f5c686b70b427de73e5d9ce6f7905a50a946abdd
Francois-Rene Rideau <fare@tunes.org>
2016-08-06 18:19:23 -0400

    Improve a few utility functions

--- util/simple-integer.lisp
+++ util/simple-integer.lisp
@@ -15,20 +15,25 @@
         finally (return m)))
 
 (defun lcm.1 (n m)
-  (* n (/ m (lcm n m))))
+  (* n (/ m (gcd n m))))
 
 (defun isqrt.1 (n)
   (check-type n (integer 0 *))
+  ;; This function computes ISQRT, using the Newton-Raphson method,
+  ;; which doubles the number of valid digits at every iteration
+  ;; (so should have only 6 iterations for a 64-bit number).
+  ;; We compute with one extra bit for the candidate square root (two extra bits for n)
+  ;; to keep termination simple while avoid bad rounding effects.
+  ;; Someone who'd want to use only fixnums when provided fixnum input might instead
+  ;; strip one bit when looking for termination, then use dichotomy for the last bit.
+  ;; Considering the speed of FPUs these days, we would probably also do better
+  ;; by using SQRT to compute the first 53 bits of mantissa instead of shifting the bits of n
+  ;; to the right.
   (if (<= n 1) n
       (loop
-	;; Compute with two extra bits for n,
-	;; to keep termination simple while avoid bad rounding effects.
-	;; Note that if we wanted to optimize the fixnum case, then
-	;; we might want to treat specially numbers too big to fit those two extra bits in a fixnum,
-	;; and determine their last bit using dichotomy.
-	:with n4 = (ash n 2)
-	:with p = (ash n4 (- 1 (ash (integer-length n) -1)))
-	:for q = (floor n4 p)
+	:with nx4 = (ash n 2)
+	:with first-approx = (ash nx4 (- 1 (ash (integer-length n) -1)))
+	:for p = first-approx :then (ash (+ p q) -1)
+	:for q = (floor nx4 p)
 	:while (> p q)
-	:do (setf p (floor (+ p q) 2))
 	:finally (return (ash p -1)))))
